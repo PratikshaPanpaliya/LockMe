31575e7acc950cf8ab5a0b2eb103f7cbca4b0ad6
Panpaliya <Pratiksha_Panpaliya@Dell.com>
2021-06-28 14:17:20 +0530

    Welcome Screen added

---
+++ src/main/java/org/example/virtualkey/screens/FileOptionsScreen.java
@@ -0,0 +1,47 @@
+package org.example.virtualkey.screens;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import org.example.virtualkey.entities.Directory;
+import org.example.virtualkey.services.ScreenService;
+
+
+public class FileOptionsScreen implements Screen {
+	
+	private Directory dir = new Directory();
+	
+	private ArrayList<String> options = new ArrayList<>();
+
+    public FileOptionsScreen() {
+    	
+    	options.add("1. Add a File");
+        options.add("2. Delete A File");
+        options.add("3. Search A File");
+        options.add("4. Return to Menu");
+        
+    }
+    
+    @Override
+    public void Show() {
+    	System.out.println("File Options Menu");
+        for (String s : options) {
+            System.out.println(s);
+        }
+
+    }
+
+    public void GetUserInput() {
+        int selectedOption;
+        while ((selectedOption = this.getOption()) != 4) {
+            this.NavigateOption(selectedOption);
+        }
+    }
+
+}
